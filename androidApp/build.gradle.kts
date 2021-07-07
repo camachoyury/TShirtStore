c8819d9eb1da72c44a0251d9b18b611e88a3dd9f
Yury Camacho <yury.camacho@rappi.com>
2021-07-07 10:59:13 -0400

    added compose support

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -5,18 +5,17 @@ plugins {
     id ("androidx.navigation.safeargs.kotlin")
 }
 
+val composeVersion = "1.0.0-rc01"
+
 dependencies {
     implementation(project(":shared"))
+    implementation("androidx.compose.runtime:runtime:$composeVersion")
     implementation("com.google.android.material:material:1.3.0")
     implementation("androidx.appcompat:appcompat:1.2.0")
     implementation("androidx.constraintlayout:constraintlayout:2.0.4")
     implementation("androidx.navigation:navigation-fragment-ktx:2.3.5")
     implementation("androidx.navigation:navigation-ui-ktx:2.3.5")
     implementation("androidx.legacy:legacy-support-v4:1.0.0")
-//    val coroutinesVersion = "1.5.0-RC"
-//    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutinesVersion")
-//    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:$coroutinesVersion")
-//    implementation("androidx.recyclerview:recyclerview:1.2.1")
 }
 
 android {
@@ -35,15 +34,30 @@ android {
     }
     buildFeatures {
         viewBinding = true
+        compose = true
+    }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = composeVersion
+        kotlinCompilerVersion = "1.5.10"
+
     }
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
-
     }
 
     kotlinOptions {
         jvmTarget = "1.8"
     }
 }
+
+//tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+//    kotlinOptions {
+//        jvmTarget = JavaVersion.VERSION_1_8.toString()
+//        freeCompilerArgs = listOf("-Xallow-jvm-ir-dependencies", "-Xskip-prerelease-check",
+//            "-Xuse-experimental=kotlinx.coroutines.ExperimentalCoroutinesApi"
+//        )
+//    }
+//}
--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ buildscript {
     }
     dependencies {
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.10")
-        classpath("com.android.tools.build:gradle:4.1.2")
+        classpath ("com.android.tools.build:gradle:7.0.0-beta05")
         classpath("org.jetbrains.kotlin:kotlin-serialization:1.5.0")
         classpath ("androidx.navigation:navigation-safe-args-gradle-plugin:2.3.2")
     }
