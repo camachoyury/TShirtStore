31b71447c4efe788a467266a55d74ea0abe822a8
Yury Camacho <yury.camacho@rappi.com>
2021-11-18 22:55:49 -0400

    Fix many dependencies issues

--- androidApp/src/main/java/com/camachoyury/tshirtstore/android/presentation/cart/ShoppingCartActivity.kt
+++ androidApp/src/main/java/com/camachoyury/tshirtstore/android/presentation/cart/ShoppingCartActivity.kt
@@ -15,11 +15,14 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.camachoyury.tshirtstore.android.R
+import com.camachoyury.tshirtstore.android.App
+
+import com.camachoyury.tshirtstore.android.data.repository.Shirt
 import com.camachoyury.tshirtstore.android.presentation.cart.ui.theme.TShirtStoreTheme
 import com.camachoyury.tshirtstore.android.presentation.composables.ShirtStoreToolBar
 
@@ -40,15 +43,83 @@ class ShoppingCartActivity : ComponentActivity() {
 
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun Content(){
+
+    Scaffold(
+        topBar = { ShirtStoreToolBar()  },
+        drawerContent = { androidx.compose.material.Text(text = "drawerContent") },
+        content = {
+            val list = App.emulatedDB
+            LazyColumn(modifier = Modifier
+                .fillMaxHeight()
+                .padding(10.dp)) {
+                items(items = list, itemContent = { item ->
+                    ShoppingCartItem(item)
+                    Divider(color = Color.Black)
+                })
+            }
+
+        },
+        bottomBar = {
+            Button(modifier = Modifier.height(56.dp)
+                .fillMaxWidth()
+                .padding(bottom = 15.dp, start = 10.dp, end = 10.dp) , onClick = { /* Do something! */ }, colors = ButtonDefaults.textButtonColors(
+                backgroundColor = Color.Black
+            )) {
+                Text("Buy ", color = Color.White)
+            }
+        }
+    )
 }
+
+
+@Composable
+fun ShoppingCartItem(shirt: Shirt) {
+    val context = LocalContext.current
+    val imageUri = "@drawable/${shirt.image}"
+    val imageResource =
+        context.resources.getIdentifier(imageUri, null, context.packageName)
+
+    Row(
+        Modifier
+            .fillMaxWidth()
+            .padding(8.dp), horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        Surface(
+            modifier = Modifier.size(100.dp),
+            shape = CircleShape,
+            color = MaterialTheme.colors.onSurface.copy(alpha = 0.2f)
+        ) {
+            Image(
+                painterResource(imageResource),
+                contentDescription = "",
+                contentScale = ContentScale.FillWidth,
+                modifier = Modifier.fillMaxSize())
+        }
+
+        Column(
+            modifier = Modifier
+                .fillMaxHeight()
+                .padding(start = 8.dp)
+                .align(Alignment.CenterVertically)
+        ) {
+            Text(shirt.title, fontWeight = FontWeight.Bold)
+            CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
+                Text("${shirt.price} $", style = MaterialTheme.typography.body2,modifier = Modifier.align(
+                    Alignment.End))
+            }
+        }
+
+
+    }
+
+}
+
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     TShirtStoreTheme {
-        Greeting("Android")
     }
 }
 
