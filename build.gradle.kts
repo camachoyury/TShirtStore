43993a42ed08b018a75eac75d78a245e07e6e4a2
yury.camacho <yury.camacho@rappi.com>
2022-11-30 20:01:50 -0400

    fixed dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ buildscript {
     }
     dependencies {
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.10")
-        classpath ("com.android.tools.build:gradle:7.0.0-beta05")
+        classpath ("com.android.tools.build:gradle:7.0.4")
         classpath("org.jetbrains.kotlin:kotlin-serialization:1.5.0")
         classpath ("androidx.navigation:navigation-safe-args-gradle-plugin:2.3.2")
     }
